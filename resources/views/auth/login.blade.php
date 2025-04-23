9d250e8841c90c2d769156a374925a5e9318d028
ifnuu01 <ifnuu01@gmail.com>
2025-04-23 20:14:49 +0800

    style : memperbaiki login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,21 +9,25 @@
         <form action="" class="flex flex-col justify-center items-center w-full md:w-1/2 p-4">
             <img src="{{asset('images/logo.svg')}}" alt="">
             <p class="text-sm text-fg text-center">Silahkan gunakan <b class="text-skyBlueDark">email</b> anda untuk masuk</p>
-            <div class="flex flex-col mt-4 w-full">
-                <label for="email" class="text-sm text-fg">Email</label>
-                <div class="flex items-center border rounded-md px-2 mt-1 focus:outline-none hover:ring-2 hover:ring-skyBlueDark">
-                    <iconify-icon icon="material-symbols:person-outline" class="text-fg" width="24" height="24"></iconify-icon>
-                    <input type="email" name="email" id="email" class="rounded-md p-2 outline-none" placeholder="email@gmail.com" required>
-                </div>
+            <x-input-with-icon
+                type="email"
+                name="email"
+                label="Username"
+                placeholder="email@gmail.com"
+                icon="material-symbols:person-outline"
+            />
+
+            <x-input-with-icon
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="******"
+                icon="mdi:password-outline"
+            />
+            <div class="mt-4 w-full">
+                <x-button text="Masuk" icon="material-symbols:login" width="w-full"/>
             </div>
-            <div class="flex flex-col mt-4 w-full">
-                <label for="email" class="text-sm text-fg">Password</label>
-                <div class="flex items-center border rounded-md px-2 mt-1 focus:outline-none hover:ring-2 hover:ring-skyBlueDark">
-                    <iconify-icon icon="mdi:password-outline" class="text-fg" width="24" height="24"></iconify-icon>
-                    <input type="password" name="email" id="email" class="rounded-md p-2 outline-none" placeholder="*****" required>
-                </div>
-            </div>
-            <button class="mt-4 w-full bg-skyBlueDark p-2 rounded-md text-white text-base ">Login</button>
+
         </form>
     </div>
 </div>

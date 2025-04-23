eaaf4ee8529a5b157e9a153867c261e821a123a8
ifnuu01 <ifnuu01@gmail.com>
2025-04-23 18:12:01 +0800

    feat : menambahkan layout

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</head>
+<body class="bg-skyBlue">
+    <div class="flex min-h-screen">
+        <aside class="w-64 p-6 fixed h-full">
+            <div class="flex items-center gap-4">
+                <img src="{{asset('images/logo.svg')}}" width="60" height="54" alt="">
+                <p class="text-lg text-fg font-semibold">LaundryNotes</p>
+            </div>
+            <nav class="space-y-4 text-fg mt-10">
+                <p class="text-fg text-sm mb-2 font-semibold">Dashboard</p>
+                <div class="ml-2">
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="material-symbols:dashboard-outline" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Beranda</a>
+                    </div>
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="lets-icons:order" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">List Pesanan</a>
+                    </div>
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="subway:time-2" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Riwayat Pesanan</a>
+                    </div>
+                </div>
+                <p class="text-fg text-sm mb-2 font-semibold">Master Data</p>
+                <div class="ml-2">
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="material-symbols:local-laundry-service-outline" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Paket Layanan</a>
+                    </div>
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="gg:profile" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Kasir</a>
+                    </div>
+                </div>
+                <p class="text-fg text-sm mb-2 font-semibold">Laporan</p>
+                <div class="ml-2">
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="lets-icons:order" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Jumlah Pemesanan</a>
+                    </div>
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="tdesign:money" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Total Pendapatan</a>
+                    </div>
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="material-symbols:local-laundry-service-outline" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Paket Terlaris</a>
+                    </div>
+                    <div class="flex items-center gap-4 hover:bg-skyBlueDark text-fg hover:text-white rounded-md p-2">
+                        <iconify-icon icon="gg:profile" width="24" height="24"></iconify-icon>
+                        <a href="#" class="block text-sm font-semibold py-2">Kinerja Kasir</a>
+                    </div>
+                </div>
+            </nav>
+            <button class="bg-skyBlueDark text-white text-semibold p-2 w-full rounded-md mt-10">Logout</button>
+          </aside>
+
+          <main class="flex-1 p-6 ml-64">
+            <div class="mb-6">
+              <h1 class="text-lg text-fg font-semibold">Selamat Datang, Syifai Matcha</h1>
+              <p class="text-sm text-fg">Selasa, 18 Maret 2025</p>
+            </div>
+            <div class="bg-white p-6 rounded-md shadow-sm mt-12">
+              @yield('content')
+            </div>
+          </main>
+    </div>
+    @stack('scripts')
+</body>
+</html>

4a53315e299f8e4cdfcc7bc0076364392fee769e
ifnuu01 <ifnuu01@gmail.com>
2025-04-25 19:33:44 +0800

    feat : menambahkan animasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,36 @@ module.exports = {
         warningDark: '#967522',
         danger: '#C53D40',
         blueDark: '#187ABA',
-    }
+    },
+    animation: {
+      'fade-right': 'fadeInRight 1s ease-out',
+      'fade-left': 'fadeInLeft 1s ease-out',
+      'fade-up': 'fadeInUp 1s ease-out',
+      'fade-down': 'fadeInDown 1s ease-out',
+      'fade-in': 'fadeIn 1s ease-out',
+    },
+    keyframes: {
+      fadeInRight: {
+        '0%': { opacity: '0', transform: 'translateX(50px)' },
+        '100%': { opacity: '1', transform: 'translateX(0)' },
+      },
+      fadeInLeft: {
+        '0%': { opacity: '0', transform: 'translateX(-50px)' },
+        '100%': { opacity: '1', transform: 'translateX(0)' },
+      },
+      fadeInUp: {
+        '0%': { opacity: '0', transform: 'translateY(50px)' },
+        '100%': { opacity: '1', transform: 'translateY(0)' },
+      },
+      fadeInDown: {
+        '0%': { opacity: '0', transform: 'translateY(-50px)' },
+        '100%': { opacity: '1', transform: 'translateY(0)' },
+      },
+      fadeIn: {
+        '0%': { opacity: '0' },
+        '100%': { opacity: '1' },
+      },
+    },
     },
   },
   plugins: [],

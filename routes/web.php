62907950ea4a05b07ac3baa395a81b7053343f88
ifnuu01 <ifnuu01@gmail.com>
2025-04-30 21:03:30 +0800

    feat : menambahkan route resource

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PaketController;
 use App\Http\Controllers\PesananController;
 use App\Http\Controllers\RiwayatController;
-use App\Http\Controllers\LayananController;
+use App\Http\Controllers\UsersController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,50 +25,13 @@ Route::resource('/', WelcomeController::class);
 Route::get('/login', [AuthController::class, 'index'])->name("login");
 Route::post('/login-proses', [AuthController::class, 'loginProses'])->name("login-proses");
 
-Route::get('dashboard/pesanan',[PesananController::class, 'index'])->name('pesanan.index');
-Route::get('/dashboard/pesanan/create', [PesananController::class, 'create'])->name('pesanan.create');
+// pake resouce untuk CRUD
+Route::resource('dashboard/pesanan', PesananController::class);
+Route::resource('dashboard/riwayat', RiwayatController::class);
+Route::resource('dashboard/layanan', PaketController::class);
 
-Route::get('/dashboard/riwayat', [RiwayatController::class, 'index'])->name('riwayat.index');
-
-Route::get('/dashboard/layanan', [LayananController::class,'index'])->name('layanan.index');
-
-Route::get('/dashboard/layanan/create', function () {
-    return view('layanan.create');
-})->name('layanan.create');
-
-Route::get('/dashboard/layanan/edit', function () {
-    return view('layanan.edit');
-})->name('layanan.edit');
-
-Route::get('/dashboard/layanan/detail', function () {
-    return view('layanan.detail');
-})->name('layanan.detail');
-
-
-Route::get('/dashboard/pesanan/edit', function () {
-    return view('pesanan.edit');
-})->name('pesanan.edit');
-
-Route::get('/dashboard/pesanan/detail', function () {
-    return view('pesanan.detail');
-})->name('pesanan.detail');
-
-Route::get('/dashboard/riwayat', function () {
-    return view('riwayat.list-riwayat');
-})->name('riwayat.index');
-
-
-Route::get('/dashboard/kasir', function () {
-    return view('kasir.list-kasir');
-})->name('kasir.index');
-
-Route::get('/dashboard/kasir/create', function () {
-    return view('kasir.create');
-})->name('kasir.create');
-
-Route::get('/dashboard/kasir/edit', function () {
-    return view('kasir.edit');
-})->name('kasir.edit');
+// User maksudnya Buat CRUD Kasir
+Route::resource('dashboard/kasir', UsersController::class);
 
 Route::get('/dashboard/jumlah-pemesanan', function () {
     return view('laporan.jumlah-pemesanan');

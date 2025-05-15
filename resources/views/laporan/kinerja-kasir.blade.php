c052e0e655b3a25436b76150234430399ca2252d
ifnuu01 <ifnuu01@gmail.com>
2025-05-15 21:46:59 +0800

    feat : menambahkan cetak kinerja kasir

--- resources/views/laporan/kinerja-kasir.blade.php
+++ resources/views/laporan/kinerja-kasir.blade.php
@@ -50,10 +50,10 @@
         <h2 class="text-lg font-semibold text-fg">Kinerja Kasir</h2>
         <p class="text-fg text-sm">Menampilkan kinerja kasir pada sistem LaundryNotes</p>
     </div>
-    <button class="bg-skyBlueDark text-white p-2 rounded-md flex items-center gap-2">
+    <a href="{{route('laporan.cetak-kinerja-kasir')}}" class="bg-skyBlueDark text-white p-2 rounded-md flex items-center gap-2">
         <iconify-icon icon="material-symbols:print-outline" width="24" height="24"></iconify-icon>
         <span>Cetak Laporan</span>
-    </button>
+    </a>
 </div>
 
 <div class="mt-4">
